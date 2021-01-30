cc6b7e876e2b4a46ea1e8bc3b4c34c00d7a2a904
Yash Gupta <hpsyash@gmail.com>
2021-01-30 17:53:15 +0530

    updated

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react' ;
 import Aux from '../../hoc/Auxi';
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Aux>
 <div>Toolbar , Sidebar, Backdrop</div>
-    <main>
+    <main className={classes.content}>
         {props.children}
     </main>
 
