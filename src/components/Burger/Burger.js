fb886d377eaeb2b541862ca2581116507e5bb8ef
Yash Gupta <hpsyash@gmail.com>
2021-01-31 20:51:16 +0530

    updated

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,21 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])]
                 .map((_, i) => {
                     return <BurgerIngredient key={igKey + i} type={igKey} />
                 });
-        });
+        })
+        .reduce((arr,el) => {
+            return arr.concat(el)
+        },[]);
+
+        if (transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please Start adding ingredients!</p>;
+        }
+    
 
     return (
         <div className={classes.Burger}>
