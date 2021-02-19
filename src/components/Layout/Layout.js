26f121e714ea0c6fc3af63b2271eae0dbc4abdbf
Yash Gupta <hpsyash@gmail.com>
2021-02-19 20:41:01 +0530

    updated

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,37 @@
-import React from 'react' ;
+import React , {Component} from 'react' ;
 import Aux from '../../hoc/Auxi';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-    <main className={classes.content}>
-        {props.children}
-    </main>
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-    </Aux>
-    
-    );
+    sideDrawerClosedHandler = () => {
+        this.setState( { showSideDrawer: false } );
+    }
 
-    export default layout;
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout;
